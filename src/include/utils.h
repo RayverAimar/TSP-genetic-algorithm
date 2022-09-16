47202cb051e9280f3c48505510d9fac93e4f9de5
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-09-16 10:59:32 -0500

    Add node name, node id & distances matrix getters

---
+++ src/include/utils.h
@@ -0,0 +1,51 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cstring>
+
+#define DISTANCES_PATH "../datasets/distances.csv"
+#define N_NODES 26
+
+typedef std::vector<std::vector<float>>     FLOAT_MATRIX;
+typedef std::vector<float>                  FLOAT_VECTOR;
+
+FLOAT_MATRIX get_distances_matrix()
+{
+    
+    FLOAT_MATRIX distances;
+    
+    std::ifstream file(DISTANCES_PATH);
+    std::string line;
+
+    while(std::getline(file, line))
+    {
+        FLOAT_VECTOR row;
+        char* ptr = strtok((char*) line.c_str(), ",");
+        while (ptr != NULL)  
+        {  
+            float number = atof(ptr);
+            row.push_back(number);
+            ptr = strtok (NULL, ",");
+        }
+        distances.push_back(row);
+    }
+    return distances;
+}
+
+char get_node_name(int id)
+{
+    return char(id + 65);
+}
+
+int get_node_id(char name)
+{
+    return int(name) - 65;
+}
+
+
+#endif //__UTILS_H__
